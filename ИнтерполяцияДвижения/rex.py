c11868867f0ece1c228cc8ec90b393f0c0e4c077
mirmik <mirmikns@yandex.ru>
2019-07-17 22:49:53 +0300

    'a'

--- ИнтерполяцияДвижения/rex.py
+++ ИнтерполяцияДвижения/rex.py
@@ -1,32 +1,79 @@
 #!/usr/bin/env python3
 
+import time
 from zencad import *
 from zencad.controllers import *
 
-class zv(zencad.controllers.Unit):
+import random
+
+import numpy
+
+class zveno1(zencad.controllers.Unit):
 	def __init__(self, l):
 		super().__init__()
 		
-		self.set_shape(cylinder(r=3, h=l))
+		m = (
+			cylinder(r=3, h=l) 
+			+ cylinder(r=3, h=6, center=True).rotateX(deg(90)).up(l)
+			+ cylinder(r=3, h=6, center=True).rotateX(deg(90))
+		)
+		self.set_shape(m)
 		
-		self.outrot = zencad.controllers.CynematicUnit(
+		self.outrot = zencad.controllers.CynematicRotator(
+			ax = (0,1,0),
 			name="zv.outrot",
 			parent=self, 
 			location=up(l))
 
+o = zencad.controllers.CynematicRotator(ax=(0,0,1))
+a = zveno1(40)
+b = zveno1(30)
+c = zveno1(20)
+d = zveno1(10)
+e = zveno1(10)
 
-a = zv(40)
-b = zv(30)
+d.relocate(rotate((0,0,1),deg(90)))
 
+o.link(a)
 a.outrot.link(b)
-a.outrot.output.relocate(rotate((0,1,0),deg(45)))
+b.outrot.link(c)
+c.outrot.link(d)
+d.outrot.link(e)
 
-a.location_update_deep()
-a.print_tree()
+o.set_coord(deg(50))
+a.outrot.set_coord(deg(45))
+b.outrot.set_coord(deg(60))
+c.outrot.set_coord(deg(-20))
+d.outrot.set_coord(deg(-30))
+
+o.location_update(deep=True)
 
 scn = zencad.Scene()
 a.bind_scene_deep(scn)
 
+x = [0,0,0,0,0]
+v = [1,1,1,1,1]
+lasttime = time.time()
+def update(wdg):
+	global lasttime
+	curtime = time.time()
+	deltatime = curtime - lasttime
+	lasttime = curtime
 
+	deltatime = deltatime * 3 / 4
 
-show(scn)
+	for i in range(len(x)):
+		v[i] += random.uniform(-deltatime*3, deltatime*3)
+		x[i] += v[i] * deltatime
+
+	o.set_coord(x[0])
+	a.outrot.set_coord(x[1])
+	b.outrot.set_coord(x[2])
+	c.outrot.set_coord(x[3])
+	d.outrot.set_coord(x[4])
+	
+	o.location_update(deep=True, view=True)
+
+	#wdg.set_eye(zencad.rotateZ(zencad.deg(deltatime*7))(wdg.eye()), orthogonal=True)
+
+show(scn, animate=update)
