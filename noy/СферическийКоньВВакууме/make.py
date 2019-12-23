c820cc442f3c18dd536104e6cdd4c36d32b03b1c
mirmik <mirmikns@yandex.ru>
2019-12-23 18:28:00 +0300

    'a'

---
+++ noy/СферическийКоньВВакууме/make.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import os
+import licant
+
+def latex(tgt, tgtname, deps, src):
+	licant.makefile(tgt, tgtname=tgtname, deps=deps, src=src, do="pdflatex -jobname={tgtname} {src}")
+
+fls = [
+		"src/main.tex",
+	]
+
+for l in fls : licant.source(l)
+
+latex(src="src/main.tex", tgt="refer.pdf", tgtname="refer",
+	deps = fls
+)
+
+@licant.routine
+def re():
+	licant.do(["refer.pdf", "build"])
+
+licant.ex("refer.pdf")
